f9c025adf6fbcb45bb5d8a6e4f7c33f342964ce5
MouadCherrat <mouad19cherrat@gmail.com>
2024-03-30 21:35:53 +0000

    initial commit

---
+++ src/main/java/com/example/kanbansystem/KanbanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanbansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kanbansystem/KanbanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanbansystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
