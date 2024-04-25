231ac35d217a2f52c239ab68b62260a08c6e0dab
MouadCherrat <mouad19cherrat@gmail.com>
2024-04-25 15:20:42 +0100

    adding web and mobile folders to main projec

--- src/main/java/com/example/kanbansystem/security/SecurityConfig.java
+++ src/main/java/com/example/kanbansystem/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+
                 .antMatchers(HttpMethod.POST,"/user").hasRole("ADMIN")
 
                 .antMatchers(HttpMethod.GET,"/api/boards/**").hasAnyRole("ADMIN","USER")
