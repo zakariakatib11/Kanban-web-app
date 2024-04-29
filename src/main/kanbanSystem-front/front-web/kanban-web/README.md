3fae2eee322ad650e99d6d406efb915eecc2fe08
khalilsouidi <khalilsouidi9@gmail.com>
2024-04-30 00:03:45 +0100

    changing angular version to Angular CLI: 16.2.14 Node: 18.16.1 npm 9.5.1

--- src/main/kanbanSystem-front/front-web/kanban-web/README.md
+++ src/main/kanbanSystem-front/front-web/kanban-web/README.md
@@ -1,6 +1,6 @@
-# KanbanSystemWeb
+# KanbanWeb
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
 ## Development server
 
--- src/main/kanbanSystem-front/front-web/kanban-web/src/app/app.component.spec.ts
+++ src/main/kanbanSystem-front/front-web/kanban-web/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'kanbanSystem-web' title`, () => {
+  it(`should have as title 'kanban-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kanbanSystem-web');
+    expect(app.title).toEqual('kanban-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kanbanSystem-web');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('kanban-web app is running!');
   });
 });
