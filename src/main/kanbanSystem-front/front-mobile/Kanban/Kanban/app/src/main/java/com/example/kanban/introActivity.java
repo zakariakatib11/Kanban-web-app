d2618f409388711545d580eebd43150afbeb7ff7
Katib <zkatib0@gmail.com>
2024-05-01 17:51:28 +0100

    Adding login and signup design for mobile

---
+++ src/main/kanbanSystem-front/front-mobile/Kanban/Kanban/app/src/main/java/com/example/kanban/introActivity.java
@@ -0,0 +1,14 @@
+package com.example.kanban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class introActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
